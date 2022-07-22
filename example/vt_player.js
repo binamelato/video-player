a99a7c3b67ad921f1363b798be0fed2d1796b586
Ilos <shahral@mail.ru>
2022-07-22 21:40:26 +0300

    fix

--- example/vt_player.js
+++ example/vt_player.js
@@ -5,15 +5,10 @@ v_store = ''; //path to trailer hranilische
 v_form = vt_player[0]; //format trailer
 v_tral = vt_player[1]; //trailer identificator
 vi_drag = false;
+vi_dd = 0;
 v_pl = document.querySelector(v_spot);//container player
 _player();
 
-/* не робит
-window.addEventListener('keydown', (e) => {
-  if(e.keyCode == 27) {
-    e.preventDefault();
-  }
-});*/
 
 v_vid = document.querySelector(".vplayer"); //player
 v_hu = document.querySelector("#v_hud"); //hud
@@ -60,39 +55,34 @@ function _player(){//постсроение плеера
 }
 function _option(){
 	//размер контролов круглых
-	dp_control = 12; //получить размер а не написать
+	dp_control = 12;
 	//радиус контролов
 	dp2_control = dp_control / 2; //6
 	prog_otX = getCoords(v_fot); //24px
 	vaLength = prog_otX[1] - prog_otX[0] - dp_control; //длинна полосы 656
 	prom_otX = getCoords(v_oluk); //98px
 	veLength = prom_otX[1] - prom_otX[0] - dp_control; //длинна полосы 88
-	//console.log(vaLength); //верная длинна
-	//console.log(veLength); //верная длинна
 }
 
 function _hover(){ //отображаем худ плеера
 	v_hu.style.display = 'block';
 	_option();
-	if(v_time.innerHTML == '00:00'){
+	if(v_time.innerHTML == '00:00'){ //NaN:NaN
 		v_time.innerHTML = videoTime(v_vid.duration); 
 	}
-	//баг, если обновление страницы было,а курсор уже был на плеере, то счетчик показывает nan:nan
-	/*setTimeout(function tik(){
-		v_time.innerHTML = videoTime(v_vid.duration);
-	}, 100);*/
 }
 function _dehover(){//скрываем худ плеера через 4 секунды
-	//setTimeout(function back(){v_hu.style.display = 'none';}, 4000); //таймер наверх
+	//нужно проверять на любую активность курсора, если ее нет запускать таймер
+	setTimeout(function back(){v_hu.style.display = 'none';}, 4000); //таймер наверх
 }
 function _volplus(){ //отображаем полосу звука
 	v_olus.style.display = 'block';
 	_option();
 }
 function _volmin(){//скрываем худ плеера через 2 секунды
-	//setTimeout(function func(){v_olus.style.display = 'none';}, 200);//таймер наверх
+	setTimeout(function func(){v_olus.style.display = 'none';}, 200);//таймер наверх
 }
-function _play(){//
+function _play(){
 	cn_pl = v_play.classList.toggle('play');
 	if(cn_pl){
 		var check_im = v_play.querySelector('img').setAttribute('src','ico/pause1.png');
@@ -103,7 +93,6 @@ function _play(){//
 	}
 }
 function _fullhd(){
-	// При изменении фулл экран / нет, надо редактировать положение карретки
 	cn_fm = v_hd.classList.toggle('full');
 	if(cn_fm){
 		var check_im = v_hd.querySelector('img').setAttribute('src','ico/fullscreen2.png');
@@ -117,8 +106,8 @@ function volumeVisMove(e){
 	if(!vi_drag){
 		return;
 	}else{
-		cordx = prom_otX[1] - dp2_control; //192
-		ncordx = prom_otX[0] + dp2_control; //104
+		cordx = prom_otX[1] - dp2_control;
+		ncordx = prom_otX[0] + dp2_control;
 		nowCord = e.pageX;
 		if(nowCord>=ncordx && nowCord<=cordx){
 			bat = veLength - (cordx - nowCord);
@@ -127,7 +116,6 @@ function volumeVisMove(e){
 			valvol = (bat*100)/veLength;
 			var volume = Math.round(valvol) / 100;
 			v_vid.volume = volume;
-			//console.log(volume);
 		}
 	}
 }
@@ -164,26 +152,24 @@ function videoChangeTime(e){ //Перематываем
 }
 function videoChangeVolum(e){ //Перематываем звук
 	var mouseDX = Math.floor(e.pageX - prom_otX[0]);
-	vavol = (mouseDX*100)/veLength;
 	var volume = Math.round(vavol) / 100;
+	vavol = (mouseDX*100)/veLength;
 	v_vid.volume = volume;
-	//cдвинуть карретку
 	v_pols.style.left = (mouseDX-dp2_control)+'px';
 }
 function _fullscreen(){
-	  if (v_pl.requestFullscreen) {
+	if (v_pl.requestFullscreen){
 		v_pl.requestFullscreen();
-	  } else if (v_pl.mozRequestFullScreen) {
+	}else if (v_pl.mozRequestFullScreen){
 		v_pl.mozRequestFullScreen();
-	  } else if (v_pl.webkitRequestFullscreen) {
+	}else if (v_pl.webkitRequestFullscreen){
 		v_pl.webkitRequestFullscreen();
-	  } else if (v_pl.msRequestFullscreen) {
+	}else if (v_pl.msRequestFullscreen){
 		v_pl.msRequestFullscreen();
-	  } else { v_pl.classList.toggle("fullscreen");
-	  }
-	  videoProgress();
-	  
-	  /*
+	}else{v_pl.classList.toggle("fullscreen");
+	}
+	videoProgress();
+	/*
 	  function (o) {
 			try {
 				(o.requestFullscreen || o.webkitrequestFullscreen || mozRequestFullscreen)();
@@ -192,32 +178,28 @@ function _fullscreen(){
 		
 		можно и без try/catch
 		(o.requestFullscreen ||… || function () {}) ()
-	  */
+	*/
 }
 function _endfullscreen(){
-	  if (document._endfullscreen) {
+	if (document._endfullscreen) {
 		document._endfullscreen();
-	  } else if (document.mozCancelFullScreen) {
+	} else if (document.mozCancelFullScreen) {
 		document.mozCancelFullScreen();
-	  } else if (document.webkitExitFullscreen) {
+	} else if (document.webkitExitFullscreen) {
 		document.webkitExitFullscreen();
-	  }
-	  videoProgress(); 
-	
+	}
+	videoProgress(); 
 }
 function keyPress(e){
 	_option();
-	var tipe = e;
-	//корректировка карретки нужна тут
 	var tempVal = (v_vid.currentTime*vaLength)/v_vid.duration;
 	v_pc.style.left = tempVal +'px'; //
-	console.log('full window mode');//detected
-	console.log(e);//detected
-   // if(e.code === "Escape"){ 
-    if(!v_pl.fullscreenEnabled){ //не робит проверка на актив фулл скрин
-		console.log('lalalalalalala');
+    if(vi_dd != 0){
+		vi_dd = vi_dd - 1;
+		v_hd.querySelector('img').setAttribute('src','ico/fullscreen1.png');
+		v_hd.classList.remove('full');
     }else{
-		console.log('трахаться !');
+		vi_dd = vi_dd + 1;
 	}
 }
 function videoTime(time){ //Рассчитываем время в секундах и минутах
@@ -235,9 +217,8 @@ function videoTime(time){ //Рассчитываем время в секунд�
 	return minutesVal + ':' + secondsVal;
 }
 function getCoords(elem){
-  var box = elem.getBoundingClientRect();
-  mass = [box.left, box.right];
-  //console.log(mass);//координаты верные
-  return mass;
+	var box = elem.getBoundingClientRect();
+	mass = [box.left, box.right];
+	return mass;
 }
 });
